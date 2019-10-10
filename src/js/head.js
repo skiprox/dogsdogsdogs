b94745b3c762e40a89460976e73a02eb832a6256
skiprox <seanmichaelscanlan@gmail.com>
2019-10-10 16:28:04 -0400

    Updating with new pages, fonts, test for touch devices

--- src/js/head.js
+++ src/js/head.js
@@ -3,6 +3,12 @@
 class Head {
 	constructor() {
 		console.log('this is blocking');
+        if (!!('ontouchstart' in window)) {
+            document.addEventListener("DOMContentLoaded", function(){
+                // Handler when the DOM is fully loaded
+                document.body.classList.add("touch");
+            });
+        }
 	}
 }
 
