25da65dab53052513ff597911fa45dfe394f30f7
skiprox <seanmichaelscanlan@gmail.com>
2019-11-11 21:01:46 -0500

    Updating with calendar

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,7 @@ require('scroll-to-plugin');
 require('timeline-lite');
 // Custom modules
 const DogsLogo = require('./modules/dogs-logo');
+const GoogleCalendar = require('./modules/google-calendar');
 
 /***********************
 *
@@ -26,6 +27,15 @@ class Main {
 	}
 	initializeModules() {
 		new DogsLogo();
+        if (document.body.classList.contains('calendar')) {
+            console.log(GoogleCalendar());
+            GoogleCalendar().init({
+                calendarUrl: "https://www.googleapis.com/calendar/v3/calendars/newschool.edu_08s6u2811f1ag7kfd9c5jmm9ho@group.calendar.google.com/events?key=AIzaSyCgCK-O7Dy0tcHw8xHmJteYm4cIS3NrHeQ",
+                past: false,
+                upcomingHeading: '<h2 class="upcoming">Upcoming events</h2>',
+                pastHeading: '',
+            });
+        }
 	}
 }
 
